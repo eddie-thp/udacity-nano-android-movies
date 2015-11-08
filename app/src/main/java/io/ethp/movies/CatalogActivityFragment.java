9e8e16f88ef660807abdd1c1565bef8e3f273259
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2015-11-08 12:14:42 -0600

    # Work 001 - Add Internet Permission - Retrieve movies list from TheMovieDB.org, storing api key externally (in gradle.properties) - Show list of movies in a grid - Configure preferences - Add picasso dependency

--- app/src/main/java/io/ethp/movies/CatalogActivityFragment.java
+++ app/src/main/java/io/ethp/movies/CatalogActivityFragment.java
@@ -1,15 +1,24 @@
 package io.ethp.movies;
 
-import android.support.v4.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class CatalogActivityFragment extends Fragment {
+
+    private static final String LOG_TAG = CatalogActivityFragment.class.getSimpleName();
+
+    private ArrayAdapter<String> mCatalogAdapter;
+    private FetchMoviesTask mFetchMoviesTask;
 
     public CatalogActivityFragment() {
     }
@@ -17,6 +26,36 @@ public class CatalogActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_catalog, container, false);
+
+        mCatalogAdapter = new ArrayAdapter<String>(getActivity(), R.layout.grid_item_movie, R.id.grid_item_movie_text);
+
+        // Setup task
+        mFetchMoviesTask = new FetchMoviesTask(mCatalogAdapter);
+
+        // Setup gridView
+        GridView catalogGridView = (GridView) rootView.findViewById(R.id.gridview_catalog);
+        catalogGridView.setAdapter(mCatalogAdapter);
+
+        fetchMovies();
+
+        // TODO load images using Picasso
+        // Picasso.with(getActivity()).load().into();
+
+        //FetchMoviesTask task = new FetchMoviesTask()
+
+        // mCatalogAdapter.addAll(Arrays.asList(movies));
+
+        return rootView;
+    }
+
+    private void fetchMovies() {
+
+        String apiKey = getString(R.string.movie_db_org_api_key);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sorting = prefs.getString(getString(R.string.pref_catalog_sorting_key), getString(R.string.pref_catalog_sorting_default));
+
+        mFetchMoviesTask.execute(apiKey, sorting);
     }
 }
---
+++ app/src/main/java/io/ethp/movies/FetchMoviesTask.java
@@ -0,0 +1,155 @@
+package io.ethp.movies;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+/**
+ * Task responsible for fetching a list of movies from "www.themoviedb.org"
+ *
+ * API documentation can be found in: https://www.themoviedb.org/documentation/api/discover
+ *
+ * // TODO receive api key externally, or getString property here ? problem = lack of context
+ *
+ * Parameters:
+ * [0] = apiKey
+ * [1] = sorting
+ */
+public class FetchMoviesTask  extends AsyncTask<String, Void, String[]> {
+
+    private static final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+    private ArrayAdapter<String> mAdapter;
+
+    public FetchMoviesTask(ArrayAdapter<String> adapter) {
+        this.mAdapter = adapter;
+    }
+
+    @Override
+    protected String[] doInBackground(String... params) {
+
+        // TODO Use parameter if passed or use default for sorting
+        // String sorting = (params.length > 0 ? param[0] : getString(R.string.pref_catalog_sorting_default));
+        String apiKey = params[0];
+        String sorting = params[1];
+
+        // http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=daf267f1cbc202e37a035fc65cef8814
+/*
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sorting = prefs.getString(getString(R.string.pref_catalog_sorting_key), getString(R.string.pref_catalog_sorting_default));
+*/
+
+        final String MDB_API_KEY_PARAM = "api_key";
+        final String SORTING_PARAM = "sort_by";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http");
+        builder.authority("api.themoviedb.org");
+        builder.appendPath("3");
+        builder.appendPath("discover");
+        builder.appendPath("movie");
+        builder.appendQueryParameter(MDB_API_KEY_PARAM, apiKey);
+        builder.appendQueryParameter(SORTING_PARAM, sorting);
+        Uri mDbApiUri = builder.build();
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String responseStr = null;
+        try {
+            URL mDbApiUrl = new URL(builder.build().toString());
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) mDbApiUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            responseStr = buffer.toString();
+
+        } catch(Exception e) {
+            Log.e(LOG_TAG, "Failed to fetch movies: " + mDbApiUri, e);
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+
+        String movies[] = processApiResponse(responseStr);
+
+        return movies;
+    }
+
+    private String[] processApiResponse(String responseStr) {
+        String movies[] = null;
+
+        final String DISCOVER_RESULTS = "results";
+        final String MOVIE_TITLE = "title";
+
+        try {
+            JSONObject responseJson = new JSONObject(responseStr);
+            JSONArray responseResults = responseJson.getJSONArray(DISCOVER_RESULTS);
+
+            movies = new String[responseResults.length()];
+
+            for(int i = 0; i< responseResults.length(); i++) {
+                JSONObject movieJSON = responseResults.getJSONObject(i);
+                movies[i] = movieJSON.getString(MOVIE_TITLE);
+            }
+        } catch(JSONException e) {
+            Log.e(LOG_TAG, "Failed parsing response json" + responseStr, e);
+        }
+
+        return movies;
+    }
+
+    @Override
+    protected void onPostExecute(String[] strings) {
+        super.onPostExecute(strings);
+
+        mAdapter.addAll(Arrays.asList(strings));
+    }
+}
